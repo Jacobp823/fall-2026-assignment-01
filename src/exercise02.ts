115e7842825207ffb072aedb7a6c56c13e76372b
jacobp823 <jacobparker823@gmail.com>
2026-08-31 12:53:24 -0400

    Submitting exercise02

--- src/exercise02.ts
+++ src/exercise02.ts
@@ -1,3 +1,19 @@
 export function transcribeDNA(dna: string): string {
-  return '';
+  const complement: { [key: string]: string } = {
+    A: 'U',
+    T: 'A',
+    C: 'G',
+    G: 'C',
+  };
+
+  let result = '';
+
+  for (const character of dna) {
+    if (!(character in complement)) {
+      throw new Error(`Invalid DNA nucleotide: ${character}`);
+    }
+    result += complement[character];
+  }
+
+  return `${result}`;
 }
