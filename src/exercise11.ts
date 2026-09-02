f14f00840499f3f8c8110695fa3c16f360ed0005
jacobp823 <jacobparker823@gmail.com>
2026-09-02 12:44:42 -0400

    Submitting exercise11

--- src/exercise11.ts
+++ src/exercise11.ts
@@ -1,4 +1,11 @@
+import { promises as fs } from 'fs';
+
 export async function logStatusToFile(
   filePath: string,
   statusMessage: string,
-): Promise<void> {}
+): Promise<void> {
+  const timestamp = new Date().toISOString();
+  const message = `${timestamp} - ${statusMessage}\n`;
+
+  await fs.appendFile(filePath, message);
+}
