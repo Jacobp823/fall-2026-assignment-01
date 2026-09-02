8b88202d980b54a89ea69b2377b5f5edf1a5224d
jacobp823 <jacobparker823@gmail.com>
2026-09-01 23:55:19 -0400

    Submitting exercise07

--- src/exercise07.ts
+++ src/exercise07.ts
@@ -1,3 +1,21 @@
+import fs from 'fs';
+
+export type Gradebook = {
+  [studentName: string]: {
+    [subject: string]: number;
+  };
+};
+
 export function calculateSubjectAverage(subject: string): number {
-  return 0;
+  const data = fs.readFileSync('data/gradebook.json', 'utf8');
+
+  const gradebook: Gradebook = JSON.parse(data);
+
+  const grades = Object.values(gradebook).filter(student => student[subject] !== undefined).map(student => student[subject]);
+
+  if (grades.length === 0) {
+    return 0;
+  }
+
+  return grades.reduce((total, grade) => total + grade, 0) / grades.length;
 }
