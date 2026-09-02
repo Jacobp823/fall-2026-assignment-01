6e0f47e2a22e17a8ff929aad7bda457b5916f8cd
jacobp823 <jacobparker823@gmail.com>
2026-09-02 13:44:23 -0400

    Submitting exercise13

--- src/exercise13.ts
+++ src/exercise13.ts
@@ -1,5 +1,19 @@
-export type TodoItem = {};
+export type TodoItem = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 export async function fetchTodoSafe(todoId: number): Promise<TodoItem | null> {
-  return null;
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+    if (response.status !== 200) {
+      return null;
+    }
+    return await response.json() as TodoItem;
+
+  } catch (error) {
+    return null;
+  }
 }
